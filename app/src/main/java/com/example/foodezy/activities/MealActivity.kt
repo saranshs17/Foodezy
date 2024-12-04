6154b6e8c256c2ab8bfd7d214569e7431d47d51a
saranshs17 <saranshmain17@gmail.com>
2024-12-05 00:11:52 +0530

    feat: add Meal Activity and UI upgrades

--- app/src/main/java/com/example/foodezy/activities/MealActivity.kt
+++ app/src/main/java/com/example/foodezy/activities/MealActivity.kt
@@ -3,14 +3,40 @@ package com.example.foodezy.activities
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 import com.example.foodezy.R
+import com.example.foodezy.databinding.ActivityMealBinding
+import com.example.foodezy.fragments.HomeFragment
 
 class MealActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMealBinding
+    private lateinit var mealId:String
+    private lateinit var mealName:String
+    private lateinit var mealThumb:String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_meal)
         enableEdgeToEdge()
+        binding = ActivityMealBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        getMealInformationFromIntent()
+        setInformationInViews()
+    }
+
+    private fun setInformationInViews() {
+        Glide.with(applicationContext)
+            .load(mealThumb)
+            .into(binding.imgMealDetail)
+
+        binding.collapsingToolbar.title=mealName
+        binding.collapsingToolbar.setCollapsedTitleTextColor(resources.getColor(R.color.white))
+        binding.collapsingToolbar.setExpandedTitleColor(resources.getColor(R.color.white))
+    }
+
+    private fun getMealInformationFromIntent(){
+        val intent = intent
+        mealId=intent.getStringExtra(HomeFragment.MEAL_ID)!!
+        mealName=intent.getStringExtra(HomeFragment.MEAL_NAME)!!
+        mealThumb=intent.getStringExtra(HomeFragment.MEAL_THUMB)!!
     }
 }
--- app/src/main/java/com/example/foodezy/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/foodezy/fragments/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.foodezy.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,7 @@ import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
+import com.example.foodezy.activities.MealActivity
 import com.example.foodezy.databinding.FragmentHomeBinding
 import com.example.foodezy.models.Meal
 import com.example.foodezy.viewmodels.HomeViewModel
@@ -15,6 +17,16 @@ import com.example.foodezy.viewmodels.HomeViewModel
 class HomeFragment : Fragment() {
     private lateinit var binding:FragmentHomeBinding
     private lateinit var homeMvvm:HomeViewModel
+    private lateinit var randomMeal:Meal
+
+    companion object{
+        const val MEAL_ID="com.example.foodezy.fragments.idMeal"
+        const val MEAL_NAME="com.example.foodezy.fragments.nameMeal"
+        const val MEAL_THUMB="com.example.foodezy.fragments.thumbMeal"
+        const val CATEGORY_NAME="com.example.foodezy.fragments.categoryName"
+        const val MEAL_STR="com.example.foodezy.fragments.strMeal"
+        const val MEAL_AREA="com.example.foodezy.fragments.strArea"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,16 +47,25 @@ class HomeFragment : Fragment() {
 
         homeMvvm.getRandomMeal()
         observerRandomMeal()
+        onRandomMealClick()
     }
-
+    private fun onRandomMealClick(){
+        binding.randomMeal.setOnClickListener {
+            val intent = Intent(activity,MealActivity::class.java)
+            intent.putExtra(MEAL_ID,randomMeal.idMeal)
+            intent.putExtra(MEAL_NAME,randomMeal.strMeal)
+            intent.putExtra(MEAL_THUMB,randomMeal.strMealThumb)
+            startActivity(intent)
+        }
+    }
     private fun observerRandomMeal(){
-        homeMvvm.observeRandomMealLiveData().observe(viewLifecycleOwner,object :  Observer<Meal>{
-            override fun onChanged(t: Meal) {
-                Glide.with(this@HomeFragment)
-                    .load(t!!.strMealThumb)
-                    .into(binding.imgRandomMeal)
-            }
-        })
+        homeMvvm.observeRandomMealLiveData().observe(viewLifecycleOwner
+        ) { meal ->
+            Glide.with(this@HomeFragment)
+                .load(meal!!.strMealThumb)
+                .into(binding.imgRandomMeal)
+            this.randomMeal=meal
+        }
     }
 
 }
